3619d5c5aa244ddcfa2fafae61d06ca710980f72
Diego Britos <diego.britos@bitlogic.io>
2020-05-14 13:15:48 -0300

    ABO-2493: Basic structure after spring initializr.

---
+++ src/main/java/ar/edu/ues21/algarrobo/admissioneventservice/AdmissionEventServiceApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.ues21.algarrobo.admissioneventservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmissionEventServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmissionEventServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/ues21/algarrobo/admissioneventservice/AdmissionEventServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package ar.edu.ues21.algarrobo.admissioneventservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdmissionEventServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

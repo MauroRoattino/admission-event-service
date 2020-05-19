40dc8c0a4ac1a3962fb3df89850cc9e77c80e9a7
Diego Britos <diego.britos@bitlogic.io>
2020-05-19 13:48:15 -0300

    ABO-2493: Setup context config class for tests.

--- src/test/java/ar/edu/ues21/algarrobo/admissioneventservice/AdmissionEventServiceApplicationTests.java
+++ src/test/java/ar/edu/ues21/algarrobo/admissioneventservice/AdmissionEventServiceApplicationTests.java
@@ -3,10 +3,12 @@ package ar.edu.ues21.algarrobo.admissioneventservice;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ContextConfiguration(classes = AdmissionEventServiceApplicationTests.class)
 public class AdmissionEventServiceApplicationTests {
 
 	@Test

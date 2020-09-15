d6b8e7ce6f05e668365e9084d19f73914157adaa
alfredo.martinez <alfredo.martinez@proveedor.ues21.edu.ar>
2020-09-15 16:24:40 -0300

    Implementation of new controllers that allows send many events

--- src/main/java/ar/edu/ues21/algarrobo/admissioneventservice/controller/ProducerController.java
+++ src/main/java/ar/edu/ues21/algarrobo/admissioneventservice/controller/ProducerController.java
@@ -1,27 +1,13 @@
 package ar.edu.ues21.algarrobo.admissioneventservice.controller;
 
-import ar.edu.ues21.algarrobo.admissioneventservice.model.Enrollment.Contact;
-import ar.edu.ues21.algarrobo.admissioneventservice.model.Enrollment.ContactAddress;
-import ar.edu.ues21.algarrobo.admissioneventservice.model.Enrollment.ContactOrigin;
-import ar.edu.ues21.algarrobo.admissioneventservice.model.Enrollment.ContactOriginId;
 import ar.edu.ues21.algarrobo.admissioneventservice.model.Enrollment.Enrollment;
-import ar.edu.ues21.algarrobo.admissioneventservice.model.Enrollment.Location;
-import ar.edu.ues21.algarrobo.admissioneventservice.model.Enrollment.Student;
 import ar.edu.ues21.algarrobo.admissioneventservice.model.User.UserContact;
-import ar.edu.ues21.algarrobo.admissioneventservice.client.admissionapi.AdmissionClient;
 import ar.edu.ues21.algarrobo.admissioneventservice.model.ClusterResponseMetadata;
-import ar.edu.ues21.algarrobo.admissioneventservice.model.EnrollmentEvent;
 import ar.edu.ues21.algarrobo.admissioneventservice.model.AcademicLife.AcademicLifeStudentRecord;
 import ar.edu.ues21.algarrobo.admissioneventservice.service.ProducerService;
 import io.swagger.annotations.*;
-import retrofit2.Response;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-
-import com.google.common.collect.Lists;
 
 import org.apache.kafka.clients.producer.RecordMetadata;
 import org.slf4j.Logger;
@@ -41,17 +27,10 @@ public class ProducerController {
 
     private final ProducerService producerService;
 
-    private static final List<ContactOrigin> ORIGIN_MASSIVE =  List.of( ContactOrigin.builder().id(ContactOriginId.builder().origin(39l).build()).build());
-    private static final List<ContactOrigin> ORIGIN_ECOMERCE = List.of(ContactOrigin.builder().id(ContactOriginId.builder().origin(38l).build()).build());
-
     @Autowired
     public ProducerController(ProducerService producerService) {
         this.producerService = producerService;
     }
-    
-    @Autowired
-    private AdmissionClient amissionClient;
-    
 
     @PreAuthorize("#oauth2.hasScope('admission-publish:write')")
     @ApiOperation(value = "Send a pre-enrollment event to Kafka cluster", response = RecordMetadata.class)
@@ -64,44 +43,23 @@ public class ProducerController {
             @RequestHeader(value = "eventType", defaultValue = "pre-enrollment-event") String eventType,
             @RequestHeader(value = "source", defaultValue = "-") String source) {
         LOGGER.info("Sending message");
-
- 
-              
-
-        for (var ticket : enrollmentEvent.getTickets()) {
-            ticket.setValorBruto(0.0);
-            ticket.setPriceId(0L);
-        }
-        Student student = enrollmentEvent.getStudentRecord().getStudent();
-
-  
-        Contact contact = student.getContact();
-
-        if (enrollmentEvent.isMassive()) {
-            contact.setOrigin(ORIGIN_MASSIVE);
-        } else {
-            contact.setOrigin(ORIGIN_ECOMERCE);
-        }
-
-        contact.getAddresses().stream()
-                .filter(a -> a.getLocationRef() == null && a.getLocationId() != null).parallel().forEach(address -> {
-                    try {
-
-                        var rep = amissionClient.getLocation(address.getLocationId()).execute();
-                        if (rep.isSuccessful()) {
-                            Location locationRef = rep.body();
-                            address.setLocationRef(locationRef);
-                        }
-                    } catch (Exception e) {
-                        LOGGER.error(
-                                "Contact Address Location INVALID : " + enrollmentEvent.getStudentRecord().getId());
-                    }
-
-                });
-
         return producerService.sendEnrollmentEvent(enrollmentEvent, eventType, source);
     }
-    
+
+    @PreAuthorize("#oauth2.hasScope('admission-publish:write')")
+    @ApiOperation(value = "Asynchronously send a list of pre-enrollment events to Kafka cluster", response = RecordMetadata.class)
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Messages sent")})
+    @PostMapping(value = "/sendManyAdmissionEvents")
+    public ResponseEntity sendManyAdmissionEvents(
+            @RequestBody List<Enrollment> enrollmentList,
+            @RequestHeader(value = "eventType", defaultValue = "pre-enrollment-event") String eventType,
+            @RequestHeader(value = "source", defaultValue = "-") String source) {
+
+        LOGGER.info("Sending {} administration pre-enrollment events to Kafka", enrollmentList.size());
+        producerService.sendManyAdmissionEvents(enrollmentList, eventType, source);
+        return ResponseEntity.ok("Messages sent");
+    }
+
     @PreAuthorize("#oauth2.hasScope('student-record-publish:write')")
     @ApiOperation(value = "Send a student-record event to Kafka cluster", response = RecordMetadata.class)
     @ApiResponses(value = {
@@ -115,8 +73,21 @@ public class ProducerController {
         LOGGER.info("Sending student-record event");
         return producerService.sendStudentRecordEvent(studentRecord, eventType, source);
     }
-    
-    
+
+    @PreAuthorize("#oauth2.hasScope('student-record-publish:write')")
+    @ApiOperation(value = "Asynchronously send a list of student record events to Kafka cluster", response = RecordMetadata.class)
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Messages sent")})
+    @PostMapping(value = "/sendManyStudentRecordEvents")
+    public ResponseEntity sendManyStudentRecordEvents(
+            @RequestBody List<AcademicLifeStudentRecord> studentRecordList,
+            @RequestHeader(value = "eventType", defaultValue = "student-record-event") String eventType,
+            @RequestHeader(value = "source", defaultValue = "-") String source) {
+
+        LOGGER.info("Sending {} studentRecord events to Kafka", studentRecordList.size());
+        producerService.sendManyStudentRecordEvents(studentRecordList, eventType, source);
+        return ResponseEntity.ok("Messages sent");
+    }
+
     @PreAuthorize("#oauth2.hasScope('contact-publish:write')")
     @ApiOperation(value = "Send a user contact event to Kafka cluster", response = RecordMetadata.class)
     @ApiResponses(value = {
@@ -129,5 +100,19 @@ public class ProducerController {
             @RequestHeader(value = "source", defaultValue = "-") String source) {
         LOGGER.info("Sending message");
         return producerService.sendUserContactEvent(contact, eventType, source);
+    }
+
+    @PreAuthorize("#oauth2.hasScope('contact-publish:write')")
+    @ApiOperation(value = "Asynchronously send a list of user contact events to Kafka cluster", response = RecordMetadata.class)
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Messages sent")})
+    @PostMapping(value = "/sendManyUserContactEvents")
+    public ResponseEntity sendManyUserContactEvents(
+            @RequestBody List<UserContact> userContacts,
+            @RequestHeader(value = "eventType", defaultValue = "contact-event") String eventType,
+            @RequestHeader(value = "source", defaultValue = "-") String source) {
+
+        LOGGER.info("Sending {} user-contact events to Kafka", userContacts.size());
+        producerService.sendManyUserContactEvents(userContacts, eventType, source);
+        return ResponseEntity.ok("Messages sent");
     }
 }
--- src/main/java/ar/edu/ues21/algarrobo/admissioneventservice/service/ProducerService.java
+++ src/main/java/ar/edu/ues21/algarrobo/admissioneventservice/service/ProducerService.java
@@ -1,19 +1,14 @@
 package ar.edu.ues21.algarrobo.admissioneventservice.service;
 
-import ar.edu.ues21.algarrobo.admissioneventservice.model.Enrollment.Enrollment;
+import ar.edu.ues21.algarrobo.admissioneventservice.client.admissionapi.AdmissionClient;
+import ar.edu.ues21.algarrobo.admissioneventservice.model.Enrollment.*;
 import ar.edu.ues21.algarrobo.admissioneventservice.model.User.UserContact;
 import ar.edu.ues21.algarrobo.admissioneventservice.engine.ProducerEngine;
-import ar.edu.ues21.algarrobo.admissioneventservice.model.AcademicOfferEvent;
-import ar.edu.ues21.algarrobo.admissioneventservice.model.CauEvent;
 import ar.edu.ues21.algarrobo.admissioneventservice.model.ClusterResponseMetadata;
 import ar.edu.ues21.algarrobo.admissioneventservice.model.EnrollmentEvent;
 import ar.edu.ues21.algarrobo.admissioneventservice.model.StudentRecordEvent;
-import ar.edu.ues21.algarrobo.admissioneventservice.model.TicketEvent;
 import ar.edu.ues21.algarrobo.admissioneventservice.model.UserContactEvent;
 import ar.edu.ues21.algarrobo.admissioneventservice.model.AcademicLife.AcademicLifeStudentRecord;
-import ar.edu.ues21.algarrobo.admissioneventservice.model.AcademicOffer.AcademicOffer;
-import ar.edu.ues21.algarrobo.admissioneventservice.model.Administration.AdministrationTicket;
-import ar.edu.ues21.algarrobo.admissioneventservice.model.Cau.Cau;
 
 import java.util.List;
 
@@ -22,11 +17,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
-import org.springframework.util.concurrent.ListenableFuture;
-import org.springframework.util.concurrent.ListenableFutureCallback;
 import org.springframework.web.context.request.async.DeferredResult;
 
 @Service
@@ -36,31 +27,42 @@ public class ProducerService {
     @Value("${kafka.topic.admission.pre_enrollment}")
     private String preEnrollmentTopic;
 
+    private static final List<ContactOrigin> ORIGIN_MASSIVE =  List.of( ContactOrigin.builder().id(ContactOriginId.builder().origin(39l).build()).build());
+    private static final List<ContactOrigin> ORIGIN_ECOMMERCE = List.of(ContactOrigin.builder().id(ContactOriginId.builder().origin(38l).build()).build());
+
     private final ProducerEngine producerEngine;
+    private final AdmissionClient admissionClient;
 
     @Autowired
-    public ProducerService(ProducerEngine producerEngine) {
+    public ProducerService(ProducerEngine producerEngine, AdmissionClient admissionClient) {
         this.producerEngine = producerEngine;
+        this.admissionClient = admissionClient;
     }
+
 
     public DeferredResult<ResponseEntity<ClusterResponseMetadata>> sendEnrollmentEvent(
             Enrollment enrollmentResponse, String eventType, String source) {
+        this.processEnrollmentEvent(enrollmentResponse);
         EnrollmentEvent enrollmentEvent = new EnrollmentEvent(enrollmentResponse, eventType, source);
         return producerEngine.sendEnrollmentEvent(enrollmentEvent);
     }
 
-    public void sendEnrollmentEvents(List<Enrollment> enrollments, String eventType, String source) {
+    public void sendManyAdmissionEvents(List<Enrollment> enrollments, String eventType, String source) {
         for (Enrollment enrollment : enrollments) {
             sendEnrollmentEvent(enrollment, eventType, source);
         }
     }
 
-   
-
     public DeferredResult<ResponseEntity<ClusterResponseMetadata>> sendUserContactEvent(
             UserContact userContact, String evenType, String source) {
         UserContactEvent userContactEvent = new UserContactEvent(userContact, evenType, source);
         return producerEngine.sendUserContactEvent(userContactEvent);
+    }
+
+    public void sendManyUserContactEvents(List<UserContact> userContactList, String eventType, String source) {
+        for (UserContact userContact : userContactList) {
+            this.sendUserContactEvent(userContact, eventType, source);
+        }
     }
 
     public DeferredResult<ResponseEntity<ClusterResponseMetadata>> sendStudentRecordEvent(
@@ -69,6 +71,42 @@ public class ProducerService {
         return producerEngine.sendStudentRecordEvent(studentRecordEvent);
     }
 
-  
+    public void sendManyStudentRecordEvents(List<AcademicLifeStudentRecord> studentRecordList, String eventType, String source) {
+        for (AcademicLifeStudentRecord studentRecord : studentRecordList) {
+            this.sendStudentRecordEvent(studentRecord, eventType, source);
+        }
+    }
 
+
+    private void processEnrollmentEvent(Enrollment enrollmentEvent) {
+        for (var ticket : enrollmentEvent.getTickets()) {
+            ticket.setValorBruto(0.0);
+            ticket.setPriceId(0L);
+        }
+        Student student = enrollmentEvent.getStudentRecord().getStudent();
+
+        Contact contact = student.getContact();
+
+        if (enrollmentEvent.isMassive()) {
+            contact.setOrigin(ORIGIN_MASSIVE);
+        } else {
+            contact.setOrigin(ORIGIN_ECOMMERCE);
+        }
+
+        contact.getAddresses().stream()
+                .filter(a -> a.getLocationRef() == null && a.getLocationId() != null).parallel().forEach(address -> {
+            try {
+
+                var rep = admissionClient.getLocation(address.getLocationId()).execute();
+                if (rep.isSuccessful()) {
+                    Location locationRef = rep.body();
+                    address.setLocationRef(locationRef);
+                }
+            } catch (Exception e) {
+                LOGGER.error(
+                        "Contact Address Location INVALID : " + enrollmentEvent.getStudentRecord().getId());
+            }
+
+        });
+    }
 }

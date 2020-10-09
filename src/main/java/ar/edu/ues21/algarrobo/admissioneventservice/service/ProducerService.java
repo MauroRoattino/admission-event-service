8d9246e79a3c17087514587ef77c8e73a755fae4
daniel.cattaneo <daniel.cattaneo@proveedor.ues21.edu.ar>
2020-10-09 14:49:02 -0300

    Added default email and telephone in contact and student record

--- src/main/java/ar/edu/ues21/algarrobo/admissioneventservice/service/ProducerService.java
+++ src/main/java/ar/edu/ues21/algarrobo/admissioneventservice/service/ProducerService.java
@@ -1,23 +1,23 @@
 package ar.edu.ues21.algarrobo.admissioneventservice.service;
 
 import ar.edu.ues21.algarrobo.admissioneventservice.client.admissionapi.AdmissionClient;
-import ar.edu.ues21.algarrobo.admissioneventservice.model.Enrollment.*;
 import ar.edu.ues21.algarrobo.admissioneventservice.engine.ProducerEngine;
+import ar.edu.ues21.algarrobo.admissioneventservice.model.AcademicLife.AcademicLifeStudentRecord;
+import ar.edu.ues21.algarrobo.admissioneventservice.model.AcademicLife.Student.AcademicLifeStudent;
+import ar.edu.ues21.algarrobo.admissioneventservice.model.Enrollment.*;
 import ar.edu.ues21.algarrobo.admissioneventservice.model.EnrollmentEvent;
 import ar.edu.ues21.algarrobo.admissioneventservice.model.StudentRecordEvent;
+import ar.edu.ues21.algarrobo.admissioneventservice.model.User.UserAddress;
 import ar.edu.ues21.algarrobo.admissioneventservice.model.User.UserData;
 import ar.edu.ues21.algarrobo.admissioneventservice.model.UserContactEvent;
-import ar.edu.ues21.algarrobo.admissioneventservice.model.AcademicLife.AcademicLifeStudentRecord;
-
-import java.util.List;
-
 import com.google.common.base.Strings;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProducerService {
@@ -31,6 +31,8 @@ public class ProducerService {
 
     private final ProducerEngine producerEngine;
     private final AdmissionClient admissionClient;
+
+    private final String defaultEmail = "notengomail@ues21.edu.ar";
 
     @Autowired
     public ProducerService(ProducerEngine producerEngine, AdmissionClient admissionClient) {
@@ -52,6 +54,24 @@ public class ProducerService {
 
     public void sendUserContactEvent(UserData userData, String evenType, String source) {
         UserContactEvent userContactEvent = new UserContactEvent(userData, evenType, source);
+
+        if (!Strings.isNullOrEmpty(userData.getPrimaryEmail()) && Strings.isNullOrEmpty(userData.getSecondaryEmail())) {
+            // primary is set, secondary empty
+            userData.setSecondaryEmail(userData.getPrimaryEmail());
+        }
+        if (!Strings.isNullOrEmpty(userData.getSecondaryEmail()) && Strings.isNullOrEmpty(userData.getPrimaryEmail())) {
+            // secondary is set, primary empty
+            userData.setPrimaryEmail(userData.getSecondaryEmail());
+        }
+
+        if (Strings.isNullOrEmpty(userData.getPrimaryEmail()) && Strings.isNullOrEmpty(userData.getSecondaryEmail())) {
+            userData.setPrimaryEmail(defaultEmail);
+            userData.setSecondaryEmail(defaultEmail);
+        }
+
+        List<UserAddress> addresses = userData.getAddresses();
+
+        setMissingTelephoneInAddress(addresses);
         producerEngine.sendUserContactEvent(userContactEvent);
     }
 
@@ -63,11 +83,31 @@ public class ProducerService {
 
     public void sendStudentRecordEvent(AcademicLifeStudentRecord studentRecord, String eventType, String source) {
         StudentRecordEvent studentRecordEvent = new StudentRecordEvent(studentRecord, eventType, source);
+
+        AcademicLifeStudent student = studentRecord.getStudent();
+        if (!Strings.isNullOrEmpty(student.getPrimaryEmail()) && Strings.isNullOrEmpty(student.getSecondaryEmail())) {
+            // primary is set, secondary empty
+            student.setSecondaryEmail(student.getPrimaryEmail());
+        }
+        if (!Strings.isNullOrEmpty(student.getSecondaryEmail()) && Strings.isNullOrEmpty(student.getPrimaryEmail())) {
+            // secondary is set, primary empty
+            student.setPrimaryEmail(student.getSecondaryEmail());
+        }
+
+        if (Strings.isNullOrEmpty(student.getPrimaryEmail()) && Strings.isNullOrEmpty(student.getSecondaryEmail())) {
+            student.setPrimaryEmail(defaultEmail);
+            student.setSecondaryEmail(defaultEmail);
+        }
+
+        List<UserAddress> addresses = student.getAddresses();
+
+        setMissingTelephoneInAddress(addresses);
+
         producerEngine.sendStudentRecordEvent(studentRecordEvent);
     }
 
     public void sendManyStudentRecordEvents(List<AcademicLifeStudentRecord> studentRecordList, String eventType,
-            String source) {
+                                            String source) {
         for (AcademicLifeStudentRecord studentRecord : studentRecordList) {
             this.sendStudentRecordEvent(studentRecord, eventType, source);
         }
@@ -93,8 +133,8 @@ public class ProducerService {
         }
 
         if (Strings.isNullOrEmpty(student.getPrimaryEmail()) && Strings.isNullOrEmpty(student.getSecondaryEmail())) {
-            student.setPrimaryEmail("no@tiene.email.com");
-            student.setSecondaryEmail("no@tiene.email.com");
+            student.setPrimaryEmail(defaultEmail);
+            student.setSecondaryEmail(defaultEmail);
         }
 
         Contact contact = student.getContact();
@@ -109,8 +149,8 @@ public class ProducerService {
         }
 
         if (Strings.isNullOrEmpty(contact.getPrimaryEmail()) && Strings.isNullOrEmpty(contact.getSecondaryEmail())) {
-            contact.setPrimaryEmail("no@tiene.email.com");
-            contact.setSecondaryEmail("no@tiene.email.com");
+            contact.setPrimaryEmail(defaultEmail);
+            contact.setSecondaryEmail(defaultEmail);
         }
 
         if (enrollmentEvent.isMassive()) {
@@ -135,5 +175,23 @@ public class ProducerService {
                     }
 
                 });
+    }
+
+    private void setMissingTelephoneInAddress(List<UserAddress> addresses) {
+        for (UserAddress address : addresses) {
+            if (!Strings.isNullOrEmpty(address.getTelephoneNumber1()) && Strings.isNullOrEmpty(address.getTelephoneNumber2())) {
+                // primary is set, secondary empty
+                address.setTelephoneNumber2(address.getTelephoneNumber1());
+            }
+            if (!Strings.isNullOrEmpty(address.getTelephoneNumber2()) && Strings.isNullOrEmpty(address.getTelephoneNumber1())) {
+                // secondary is set, primary empty
+                address.setTelephoneNumber1(address.getTelephoneNumber2());
+            }
+
+            if (Strings.isNullOrEmpty(address.getTelephoneNumber1()) && Strings.isNullOrEmpty(address.getTelephoneNumber2())) {
+                address.setTelephoneNumber1("N/A");
+                address.setTelephoneNumber2("N/A");
+            }
+        }
     }
 }
